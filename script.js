7cf72263a1b1c23ff825286627c9d024b1dd8629
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2023-08-24 09:52:06 -0600

    functions as intended

--- script.js
+++ script.js
@@ -34,20 +34,30 @@ function generatePassword() {
   };
 
   for (rule in charTypes) {
-    const userPref = prompt(
-      "(true/false) Would you like your password to include the following character type: " +
-        rule,
-      charTypes[rule]
+    // const userPref = prompt(
+    //   "(true/false) Would you like your password to include the following character type: " +
+    //     rule +
+    //     "?",
+    //   charTypes[rule]
+    // );
+    // if (typeof userPref === "boolean") {
+    //   charTypes[rule] = userPref;
+    // } else {
+    //   alert(
+    //     "Invalid (non-boolean) response, using default value: " +
+    //       charTypes[rule]
+    //   );
+    // }
+    const userPref = confirm(
+      `current rule for inclusion of ${rule} characters is ${charTypes[rule]}.
+      click "OK" to toggle this rule or "cancel" to keep current value`
     );
-    if (typeof userPref === "boolean") {
-      charTypes[rule] = userPref;
-    } else {
-      alert(
-        "Invalid (non-boolean) response, using default value: " +
-          charTypes[rule]
-      );
+    if (userPref) {
+      charTypes[rule] = !charTypes[rule];
     }
   }
+
+  console.log(charTypes);
 
   // for each desired password character:
   // - pick a type of character
@@ -56,5 +66,38 @@ function generatePassword() {
   // in the long term i would probably opt to overload the generatePassword function with one that accepts the necessary arguments
   // i will not be doing that here because this does not need to work in the long term, and i value my time
 
+  const charSets = {
+    lowercase: "abcdefghijklmnopqrstuvwxyz",
+    uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    numeric: "0123456789",
+    special: " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~",
+  };
+
+  let charMasterSet = "";
+  for (set in charSets) {
+    if (charTypes[set]) {
+      charMasterSet += charSets[set];
+    }
+    console.log(charMasterSet);
+  }
+
+  for (let i = 0; i < passLength; i++) {
+    //generate a character
+    const nextCharIndex = Math.random() * charMasterSet.length;
+    const nextChar = String(charMasterSet.charAt(nextCharIndex));
+
+    result += nextChar;
+    console.log(result);
+  }
+
   return result;
 }
+
+// function chooseCharacter(charTypes) {
+//   const charSets = {
+//     lowercase: "abcdefghijklmnopqrstuvwxyz",
+//     uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+//     numeric: "0123456789",
+//     special: " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~",
+//   };
+// }
